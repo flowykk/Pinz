a5d8bf374356ef17412ca6f29041f9db02698f44
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2026-01-13 08:49:43 +0300

    PINZ-8: Implement app colors (without dark theme)

--- Pinz/Modules/PinzUI/Project.swift
+++ Pinz/Modules/PinzUI/Project.swift
@@ -11,6 +11,7 @@ let project = Project(
             deploymentTargets: .iOS("17.0"),
             infoPlist: .default,
             sources: ["Sources/**"],
+            resources: ["Resources/**"],
             dependencies: []
         )
     ]
