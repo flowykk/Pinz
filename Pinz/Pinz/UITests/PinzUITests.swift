1276dcf4dd21f723ac508e85e665500ccbe99820
ddrakhmanov <cristgames123@gmail.com>
2026-03-18 19:01:00 +0300

    PINZ-109: Added xctestplan for future ui-tests + Vapor dependency

---
+++ Pinz/Pinz/UITests/PinzUITests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+final class PinzUITests: XCTestCase {
+
+    private var app: XCUIApplication!
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+
+    override func tearDown() {
+        app = nil
+        super.tearDown()
+    }
+}
