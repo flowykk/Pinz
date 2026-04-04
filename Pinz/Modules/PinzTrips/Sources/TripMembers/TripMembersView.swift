261af1747c3529ec50b490fcd2c28c15db804ed5
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2026-04-04 11:42:43 +0300

    PINZ-123: Added UI for trip members screen

--- Pinz/Modules/PinzTrips/Sources/TripMembers/TripMembersView.swift
+++ Pinz/Modules/PinzTrips/Sources/TripMembers/TripMembersView.swift
@@ -12,20 +12,47 @@ public struct TripMembersView: View {
     }
 
     public var body: some View {
-        VStack(spacing: 0) {
-            Header(leftView: {
-                PinzButton(
-                    type: .icon(.chevronLeft),
-                    tint: PinzUIAsset.textPrimary.swiftUIColor,
-                    action: .plain { viewModel.dispatch(.navigate(.back)) }
-                )
-            }, centerView: {
-                HeaderTitle("Участники путешествия")
-            })
+        CollapsibleHeader(needsBlur: true) {
+            header
+        } content: {
+            VStack(spacing: 8) {
+                SettingsGroup(settings: [
+                    .textField(Setting.TextFieldSetting(
+                        id: "memberSearch",
+                        text: $viewModel.searchText,
+                        placeholder: "Поиск по участникам"
+                    ))
+                ])
+
+                if viewModel.isLoading {
+                    LoadingView()
+                } else {
+                    SettingsGroup(settings: viewModel.filteredMembers.map { member in
+                        .default(Setting.DefaultSetting(
+                            id: member.id,
+                            leading: .imageTitle(member.avatar, member.username)
+                        ))
+                    }).animation(.default, value: viewModel.filteredMembers)
+                }
+            }
+            .padding(.top, 8)
+            .padding(.horizontal, 12)
 
             Spacer()
         }
         .background(PinzUIAsset.background.swiftUIColor)
         .onAppear { viewModel.setRouter(router) }
+    }
+
+    private var header: some View {
+        Header(leftView: {
+            PinzButton(
+                type: .icon(.chevronLeft),
+                tint: PinzUIAsset.textPrimary.swiftUIColor,
+                action: .plain { viewModel.dispatch(.navigate(.back)) }
+            )
+        }, centerView: {
+            HeaderTitle("Участники путешествия")
+        })
     }
 }
